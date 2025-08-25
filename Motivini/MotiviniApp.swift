598d67116179b1121b316822413ccda3169edf2d
James Di Cesare <james.dicesare9@gmail.com>
2025-08-25 08:07:52 -0400

    Initial Commit

---
+++ Motivini/MotiviniApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiviniApp.swift
+//  Motivini
+//
+//  Created by James Di Cesare on 2025-08-25.
+//
+
+import SwiftUI
+
+@main
+struct MotiviniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
